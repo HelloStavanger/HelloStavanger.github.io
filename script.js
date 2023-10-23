6e917baa39c5947ba70a96c6f77d1e36c81c8fcf
Glenn F. Henriksen <gh@justify.no>
2023-10-23 20:14:26 +0200

    Fikset litt

--- script.js
+++ script.js
@@ -1,56 +1,122 @@
-const textElement = document.querySelector(".textElement");
-const fillElement = document.querySelector(".fillElement");
+"use strict";
+
+const textElement = document.createElement("span");
+const fillElement = document.createElement("span");
+
+const additionalDiv = document.querySelector(".additional-div");
+const bodyElement = document.querySelector("body");
+
 const originalText = ' World");';
-const newText = ' Stavanger");';
+const stavanger = "Stavanger";
 const initialText = 'printf("Hello World");';
 
-let phase = "erasing";
 let eraseIndex = originalText.length;
 let typeIndex = 0;
-let spaces = Array(28).join("&nbsp;");
 const typeWriterPause = 200;
+
+const bufferLength = 28;
+let buffer = Array(bufferLength).join(" ");
+let cursorIndex = 0;
+let cursor = document.createElement("span");
+cursor.classList.add("cursor");
+cursor.innerHTML = "&nbsp;";
+
+const displayElement = document.querySelector(".console-text");
+displayElement.appendChild(textElement);
+displayElement.appendChild(cursor);
+displayElement.appendChild(fillElement);
+printBuffer();
+
+function printBuffer() {
+  textElement.innerHTML = buffer.slice(0, cursorIndex);
+  if (
+    buffer[cursorIndex] === " " ||
+    buffer[cursorIndex] === undefined ||
+    buffer[cursorIndex] === ""
+  ) {
+    cursor.innerHTML = "&nbsp;";
+  } else {
+    cursor.innerHTML = buffer[cursorIndex];
+  }
+  fillElement.innerHTML = buffer.slice(cursorIndex + 1).replace(/ /g, "&nbsp;");
+}
 
 function typeInitialText() {
   if (typeIndex < initialText.length) {
     // Type the text
-    textElement.textContent = initialText.slice(0, typeIndex + 1);
+    buffer =
+      buffer.slice(0, typeIndex) +
+      initialText.slice(typeIndex, typeIndex + 1) +
+      buffer.slice(typeIndex + 1);
     typeIndex++;
-    spaces = Array(28 - textElement.textContent.length).join("&nbsp;");
-    fillElement.innerHTML = spaces;
+    cursorIndex++;
+    printBuffer();
     setTimeout(typeInitialText, typeWriterPause);
   } else {
-    typeIndex = 0;
-    setTimeout(typeWriterEffect, 2000);
+    setTimeout(typeWriterEffect, 1000);
   }
 }
 
+let phase = "backspacing";
 function typeWriterEffect() {
-  if (phase === "erasing") {
-    if (eraseIndex > 0) {
+  if (phase === "backspacing") {
+    cursorIndex--;
+    printBuffer();
+    if (cursorIndex > 19) {
+      setTimeout(typeWriterEffect, typeWriterPause * 4);
+    } else {
+      phase = "erasing";
+      setTimeout(typeWriterEffect, typeWriterPause * 2);
+    }
+  } else if (phase === "erasing") {
+    if (cursorIndex > 13) {
       // Erase one character
-      textElement.textContent =
-        'printf("Hello' + originalText.slice(0, eraseIndex);
-      eraseIndex--;
-      spaces = Array(28 - textElement.textContent.length).join("&nbsp;");
-      fillElement.innerHTML = spaces;
-      setTimeout(typeWriterEffect, typeWriterPause);
+      cursorIndex--;
+      buffer =
+        buffer.slice(0, cursorIndex) + buffer.slice(cursorIndex + 1) + " ";
+      printBuffer();
+      setTimeout(typeWriterEffect, typeWriterPause * 2);
     } else {
       phase = "typing";
+      typeIndex = 0;
       setTimeout(typeWriterEffect, typeWriterPause);
     }
   } else if (phase === "typing") {
-    if (typeIndex < newText.length) {
-      textElement.textContent += newText.charAt(typeIndex);
-      spaces = Array(29 - textElement.textContent.length).join("&nbsp;");
-      fillElement.innerHTML = spaces;
+    if (typeIndex < stavanger.length) {
+      buffer =
+        buffer.slice(0, cursorIndex) +
+        stavanger.slice(typeIndex, typeIndex + 1) +
+        buffer.slice(cursorIndex, bufferLength - 2);
       typeIndex++;
+      cursorIndex++;
+      printBuffer();
       setTimeout(typeWriterEffect, typeWriterPause);
     } else {
-      const additionalDiv = document.querySelector(".additional-div");
-      setTimeout(() => {
-        additionalDiv.classList.add("fade-in");
-      }, 500);
+      phase = "backspacingAgain";
+      setTimeout(typeWriterEffect, typeWriterPause);
     }
+  } else if (phase === "backspacingAgain") {
+    cursorIndex--;
+    printBuffer();
+    if (cursorIndex > 8) {
+      setTimeout(typeWriterEffect, typeWriterPause);
+    } else {
+      phase = "hashtag";
+      setTimeout(typeWriterEffect, typeWriterPause * 2);
+    }
+  } else if (phase === "hashtag") {
+    buffer =
+      buffer.slice(0, cursorIndex) +
+      "#" +
+      buffer.slice(cursorIndex, bufferLength - 2);
+    typeIndex++;
+    cursorIndex++;
+    printBuffer();
+    phase = "end";
+    setTimeout(typeWriterEffect, typeWriterPause);
+  } else if (phase === "end") {
+    cursorIndex = buffer.length - 1;
+    printBuffer();
   }
 }
 
@@ -67,6 +133,8 @@ document
   });
 
 window.onload = function () {
-  fillElement.innerHTML = spaces;
-  setTimeout(typeInitialText, 2000);
+  setTimeout(typeInitialText, 500);
+  setTimeout(() => {
+    additionalDiv.classList.add("fade-in");
+  }, 750);
 };
